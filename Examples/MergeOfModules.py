8745c53ab7f1c4a5c54dea18616a4227b3883a86
DariaHinz <44225822+DariaHinz@users.noreply.github.com>
2021-01-09 14:16:20 +0100

    Update MergeOfModules.py

--- Examples/MergeOfModules.py
+++ Examples/MergeOfModules.py
@@ -1,12 +1,5 @@
-from GUI.GUI_BeesDevelopment import *
+from Modules.FuzzyLogic.Logic import Logic
+############ START SIMULATION ###########
 
-##########################TODO:#############################
-##Przygotowane do połączenia modelu logiki i rozwoju pszczół
-simulation = Simulation()
-for day in range(1, simulate_days):
-    #eggs = output logika
-    simulation.simple_run(eggs)
-
-# Na koniec możliwość wyświetlenia wykresu po całej symulacji
-simulation.window_to_generate_sumup()
-window.mainloop()
+logic = Logic()
+logic.simulate_logic()
